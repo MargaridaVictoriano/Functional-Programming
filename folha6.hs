180180637137a6baa9fcfa84044a146478712858
Margarida Victoriano <38563697+MargaridaVictoriano@users.noreply.github.com>
2019-03-28 10:54:22 +0000

    Update folha6.hs

--- folha6.hs
+++ folha6.hs
@@ -1,17 +1,7 @@
---ex48
+--ex48 : Escrever [x^2+1 | x <- [1..100], x 'mod' 3 /= 0] usando map e filter e [1..100]
 -- map f (filter p xs)
 --ou
 -- (map f . filter p) xs
-
---ex51
-myzipWith :: (a->b->c) -> [a] -> [b] -> [c]
-myzipWith f xs ys = [f x y | (x,y) <- zip xs ys]
-
-rzipWith :: (a->b->c) -> [a] -> [b] -> [c]
-rzipWith _ [] _ = []
-rzipWith _ _ [] = []
-rzipWith f (x:xs) (y:ys) = f x y : rzipWith f xs ys
-
 
 --ex49
 
@@ -33,3 +23,25 @@ myreverse' l = foldr f [] l
 
 lreverse' l = foldl f [] l
             where f y x = x:y
+
+myElem :: Eq a => a -> [a] -> Bool
+myElem n (x:xs) = any (==n) xs
+
+--ex50
+
+dec2int :: [Int] -> Int
+dec2int xs = foldr (\x l -> (l*10 + x)) (0) (reverse xs)
+
+toInt :: [Int] -> Int
+toInt = foldl addDigit 0
+   where addDigit num d = 10*num + d
+
+
+--ex51
+myzipWith :: (a->b->c) -> [a] -> [b] -> [c]
+myzipWith f xs ys = [f x y | (x,y) <- zip xs ys]
+
+rzipWith :: (a->b->c) -> [a] -> [b] -> [c]
+rzipWith _ [] _ = []
+rzipWith _ _ [] = []
+rzipWith f (x:xs) (y:ys) = f x y : rzipWith f xs ys
